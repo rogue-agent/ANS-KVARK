1fdb50243647ac863befca1760957c4da3ce11f9
Rogue <admiral.trench@pm.me>
2021-06-14 09:36:13 +0200

    b.001.21.06
    Cleaned and added all other files. Almost fully functional.

--- ANS.ps1
+++ ANS.ps1
@@ -1,7 +1,7 @@
-﻿cls
-echo "Welcome to Automatic Network Script! My name is Kvark, how may I help you?"
+﻿Clear-Host
+Write-Output "Welcome to Automatic Network Script! My name is Kvark, how may I help you?"
 pause
-cls
+Clear-Host
 Do {
 @'
 
@@ -39,7 +39,7 @@ Do {
 6. Microsoft Office
 7. Java Runtime Enviroment
 8. Java Development Kit
-9. NetPhone
+9. NetPhone Client
 10. PDF24 Creator
 11. PDF-XChange
 12. Teamviewer Host
@@ -71,6 +71,40 @@ Do {
                                 }
                             } Until ($1InnerMenu -eq 'B')
                         }
+                    '2' {
+                            Do {
+@'
+
+--------------Software Installation-------------
+1. Classic Shell 64-bit
+2. Classic Shell
+------------------------------------------------
+
+'@
+                                $1InnerMenu = Read-Host -Prompt 'Enter 1 - 3 or B for Back'
+                                Switch ($1InnerMenu) {
+                                 '1' {Start-Process -FilePath "$env:softinsta\7z1900-x64.exe"; break}
+                                 '2' {Start-Process -FilePath "$env:softinsta\7z1900.exe"; break}
+                                }
+                            } Until ($1InnerMenu -eq 'B')
+                        }
+                    '3' {
+                            Do {
+@'
+
+--------------Software Installation-------------
+1. Crystal Runtime 64-bit
+2. Crystal Runtime
+------------------------------------------------
+
+'@
+                                $1InnerMenu = Read-Host -Prompt 'Enter 1 - 3 or B for Back'
+                                Switch ($1InnerMenu) {
+                                 '1' {Start-Process -FilePath "$env:softinsta\7z1900-x64.exe"; break}
+                                 '2' {Start-Process -FilePath "$env:softinsta\7z1900.exe"; break}
+                                }
+                            } Until ($1InnerMenu -eq 'B')
+                        }
                     '4' {
                             Do {
 @'
@@ -88,6 +122,229 @@ Do {
                                 }
                             } Until ($1InnerMenu -eq 'B')
                         }
+                    '5' {
+                            Do {
+@'
+
+--------------Software Installation-------------
+1. Mozilla Firefox
+2. Mozilla Firefox Developer Edition
+------------------------------------------------
+
+'@
+                                $1InnerMenu = Read-Host -Prompt 'Enter 1 - 3 or B for Back'
+                                Switch ($1InnerMenu) {
+                                 '1' {Start-Process -FilePath "$env:softinsta\7z1900-x64.exe"; break}
+                                 '2' {Start-Process -FilePath "$env:softinsta\7z1900.exe"; break}
+                                }
+                            } Until ($1InnerMenu -eq 'B')
+                        }
+                    '6' {
+                            Do {
+@'
+
+--------------Software Installation-------------
+1. Microsoft Office 64-bit
+2. Microsoft Office 32-bit
+------------------------------------------------
+
+'@
+                                $1InnerMenu = Read-Host -Prompt 'Enter 1 - 3 or B for Back'
+                                Switch ($1InnerMenu) {
+                                 '1' {Start-Process -FilePath "$env:softinsta\7z1900-x64.exe"; break}
+                                 '2' {Start-Process -FilePath "$env:softinsta\7z1900.exe"; break}
+                                }
+                            } Until ($1InnerMenu -eq 'B')
+                        }
+                    '7' {
+                            Do {
+@'
+
+--------------Software Installation-------------
+1. Java Runtime Enviroment 8 64-bit
+2. Java Runtime Enviroment 8 32-bit
+------------------------------------------------
+
+'@
+                                $1InnerMenu = Read-Host -Prompt 'Enter 1 - 3 or B for Back'
+                                Switch ($1InnerMenu) {
+                                 '1' {Start-Process -FilePath "$env:softinsta\7z1900-x64.exe"; break}
+                                 '2' {Start-Process -FilePath "$env:softinsta\7z1900.exe"; break}
+                                }
+                            } Until ($1InnerMenu -eq 'B')
+                        }
+                    '8' {
+                            Do {
+@'
+
+--------------Software Installation-------------
+1. Java Development kit 8 64-bit
+2. Java Development kit 8 32-bit
+3. Java Development kit 11 64-bit
+------------------------------------------------
+
+'@
+                                $1InnerMenu = Read-Host -Prompt 'Enter 1 - 3 or B for Back'
+                                Switch ($1InnerMenu) {
+                                 '1' {Start-Process -FilePath "$env:softinsta\7z1900-x64.exe"; break}
+                                 '2' {Start-Process -FilePath "$env:softinsta\7z1900.exe"; break}
+                                }
+                            } Until ($1InnerMenu -eq 'B')
+                        }
+                    '9' {
+                            Do {
+@'
+
+--------------Software Installation-------------
+1. NetPhone Client 64-bit
+2. NetPhone Client
+------------------------------------------------
+
+'@
+                                $1InnerMenu = Read-Host -Prompt 'Enter 1 - 3 or B for Back'
+                                Switch ($1InnerMenu) {
+                                 '1' {Start-Process -FilePath "$env:softinsta\7z1900-x64.exe"; break}
+                                 '2' {Start-Process -FilePath "$env:softinsta\7z1900.exe"; break}
+                                }
+                            } Until ($1InnerMenu -eq 'B')
+                        }
+                    '10' {
+                            Do {
+@'
+
+--------------Software Installation-------------
+1. PDF24 Creator 10.0.12 64-bit
+2. PDF24 Creator 9.2.2 32-bit
+------------------------------------------------
+
+'@
+                                $1InnerMenu = Read-Host -Prompt 'Enter 1 - 3 or B for Back'
+                                Switch ($1InnerMenu) {
+                                 '1' {Start-Process -FilePath "$env:softinsta\7z1900-x64.exe"; break}
+                                 '2' {Start-Process -FilePath "$env:softinsta\7z1900.exe"; break}
+                                }
+                            } Until ($1InnerMenu -eq 'B')
+                        }
+                    '11' {
+                            Do {
+@'
+
+--------------Software Installation-------------
+1. PDF-XChange 64-bit
+2. PDF-XChange
+------------------------------------------------
+
+'@
+                                $1InnerMenu = Read-Host -Prompt 'Enter 1 - 3 or B for Back'
+                                Switch ($1InnerMenu) {
+                                 '1' {Start-Process -FilePath "$env:softinsta\7z1900-x64.exe"; break}
+                                 '2' {Start-Process -FilePath "$env:softinsta\7z1900.exe"; break}
+                                }
+                            } Until ($1InnerMenu -eq 'B')
+                        }
+                    '12' {
+                            Do {
+@'
+
+--------------Software Installation-------------
+1. Teamviewer Host 64-bit
+2. Teamviewer Host
+------------------------------------------------
+
+'@
+                                $1InnerMenu = Read-Host -Prompt 'Enter 1 - 3 or B for Back'
+                                Switch ($1InnerMenu) {
+                                 '1' {Start-Process -FilePath "$env:softinsta\7z1900-x64.exe"; break}
+                                 '2' {Start-Process -FilePath "$env:softinsta\7z1900.exe"; break}
+                                }
+                            } Until ($1InnerMenu -eq 'B')
+                        }
+                    '13' {
+                            Do {
+@'
+
+--------------Software Installation-------------
+1. Acriba 64-bit
+2. Acriba
+------------------------------------------------
+
+'@
+                                $1InnerMenu = Read-Host -Prompt 'Enter 1 - 3 or B for Back'
+                                Switch ($1InnerMenu) {
+                                 '1' {Start-Process -FilePath "$env:softinsta\7z1900-x64.exe"; break}
+                                 '2' {Start-Process -FilePath "$env:softinsta\7z1900.exe"; break}
+                                }
+                            } Until ($1InnerMenu -eq 'B')
+                        }
+                    '14' {
+                            Do {
+@'
+
+--------------Software Installation-------------
+1. David Client 64-bit
+2. David Client
+------------------------------------------------
+
+'@
+                                $1InnerMenu = Read-Host -Prompt 'Enter 1 - 3 or B for Back'
+                                Switch ($1InnerMenu) {
+                                 '1' {Start-Process -FilePath "$env:softinsta\7z1900-x64.exe"; break}
+                                 '2' {Start-Process -FilePath "$env:softinsta\7z1900.exe"; break}
+                                }
+                            } Until ($1InnerMenu -eq 'B')
+                        }
+                    '15' {
+                            Do {
+@'
+
+--------------Software Installation-------------
+1. ESET 64-bit
+2. ESET
+------------------------------------------------
+
+'@
+                                $1InnerMenu = Read-Host -Prompt 'Enter 1 - 3 or B for Back'
+                                Switch ($1InnerMenu) {
+                                 '1' {Start-Process -FilePath "$env:softinsta\7z1900-x64.exe"; break}
+                                 '2' {Start-Process -FilePath "$env:softinsta\7z1900.exe"; break}
+                                }
+                            } Until ($1InnerMenu -eq 'B')
+                        }
+                    '16' {
+                            Do {
+@'
+
+--------------Software Installation-------------
+1. Addison 64-bit
+2. Addison
+------------------------------------------------
+
+'@
+                                $1InnerMenu = Read-Host -Prompt 'Enter 1 - 3 or B for Back'
+                                Switch ($1InnerMenu) {
+                                 '1' {Start-Process -FilePath "$env:softinsta\7z1900-x64.exe"; break}
+                                 '2' {Start-Process -FilePath "$env:softinsta\7z1900.exe"; break}
+                                }
+                            } Until ($1InnerMenu -eq 'B')
+                        }
+                    '17' {
+                            Do {
+@'
+
+--------------Software Installation-------------
+1. VLC 64-bit
+2. VLC
+------------------------------------------------
+
+'@
+                                $1InnerMenu = Read-Host -Prompt 'Enter 1 - 3 or B for Back'
+                                Switch ($1InnerMenu) {
+                                 '1' {Start-Process -FilePath "$env:softinsta\7z1900-x64.exe"; break}
+                                 '2' {Start-Process -FilePath "$env:softinsta\7z1900.exe"; break}
+                                }
+                            } Until ($1InnerMenu -eq 'B')
+                        }
+                    
                 }
             } Until ($1MainMenu -eq 'B')
         }
